04f3d363a14deb9f4bf98a938fd6bf98d55eeccd
Kleimeri <klimopjesys@gmail.com>
2023-12-27 16:42:32 +0300

    add

--- js/catalog/catalog.js
+++ js/catalog/catalog.js
@@ -10,8 +10,8 @@ class catalog {
     blockCount.forEach(({ image, link }) => {
 
       createBody += `
-            <div class="portfo_block_ hover:border-[#00ffff] border-[#ffffff33] border-[2px] rounded-[40px] h-[480px] max-[782px]:h-[450px] max-[375px]:h-[400px] max-[320px]:h-[380px] w-[400px] max-[782px]:w-[400px] max-[375px]:w-[350px] max-[320px]:w-[300px] overflow-hidden flex justify-center items-center" style="transition:0.5s;">
-              <a href="${link}" class="${image.class_img} w-full backdrop-blur-[5px] h-[900px] rounded-[40px] hover:bg-[rgba(0,0,0,0.30)]" style="transition:0.3s; ${image.styleBgSize}"></a>
+            <div class="portfo_block_ overflow-hidden flex justify-center items-center drop-shadow-[0px_15px_5px_rgba(0,0,0,0.5)] backdrop-blur-[5px] hover:border-[#00ffff] border-[#ffffff33] border-[2px] rounded-[40px] h-[480px] max-[782px]:h-[450px] max-[375px]:h-[400px] max-[320px]:h-[380px] w-[400px] max-[782px]:w-[400px] max-[375px]:w-[350px] max-[320px]:w-[300px]" style="transition:0.5s;">
+              <a href="${link}" class="${image.class_img} w-full  h-[900px] rounded-[40px] " style="transition:0.3s; ${image.styleBgSize}"></a>
             </div>
             `;
     });
